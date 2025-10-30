61180d58a1bcb7551843aad5463947c1e20f6493
Gift <giftibe62@gmail.com>
2025-10-30 15:30:47 +0100

    feat: added require folders and setups

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import './types/express.types'; 
+// import router from './routes/indexRoutes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// app.use('/api', router);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/services/user.service.ts
@@ -0,0 +1,7 @@
+import prisma from "../../prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { Prisma, User } from "@prisma/client";
+
+class UserService {}
+
+export default new UserService();
