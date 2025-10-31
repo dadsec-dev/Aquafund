69ded909216a3477dd2e85c3715893cbe470312f
Gift <giftibe62@gmail.com>
2025-10-31 13:13:33 +0100

    chore: task listing

---
+++ src/services/project.service.ts
@@ -0,0 +1,19 @@
+import prisma from "../prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { Prisma, User } from "@prisma/client";
+
+class ProjectService {
+
+// POST - /api/projects - Create a new project (only verified NGOs/admins).
+
+// GET - /api/projects -List all projects with filters (status, city, creator, etc.).
+
+// GET - /api/projects/:id - Get details of a specific project.
+
+// PUT - /api/projects/:id - Update project details (admin/creator only).
+
+// PATCH - /api/projects/:id/status - Change project status (PENDING, APPROVED, COMPLETED, etc.).
+
+// DELETE - /api/projects/:id - Delete a project (admin only).
+
+}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,23 @@
-import prisma from "../../prisma/client";
+import prisma from "../prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { Prisma, User } from "@prisma/client";
 
-class UserService {}
+class UserService {
+    // POST - /api/auth/register -Register a new user (donor or NGO).
+
+    // POST - /api/auth/login - Authenticate user and return JWT token.
+
+    // GET - /api/users - (Admin only) List all users.
+
+    // GET - /api/users/:id - Get details of a single user by ID.
+
+    // PUT - /api/users/:id - Update user information.
+
+    // DELETE - /api/users/:id - Delete a user account (admin only).
+
+    // PATCH - /api/users/:id/verify - Update user verification status (verified, pending, etc.).
+    
+    
+}
 
 export default new UserService();
