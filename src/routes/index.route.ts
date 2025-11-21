f639eb472d4b851af547583f46e768ba99c2232d
Gift <giftibe62@gmail.com>
2025-11-21 13:20:46 +0100

    chore:route-linked

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,12 +3,13 @@ import userRoutes from "./user.route";
 import projectRoutes from "./project.route";
 import locationRoutes from "./location.route";
 import authRoute from "./auth.route"
-
+import ngoRoutes from "./ngo.route";
 const router = Router();
 
 router.use("/v1", userRoutes);
 router.use("/v1", projectRoutes);
 router.use("/v1", locationRoutes);
 router.use("/v1", authRoute)
+router.use("/v1", ngoRoutes);
 
 export default router;
